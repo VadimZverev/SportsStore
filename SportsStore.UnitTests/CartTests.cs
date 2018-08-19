cdc877c312ab46cf996a8f190bf1137e52dfc320
Вадим Зверев <WadimZverev@gmail.com>
2018-08-19 18:22:10 +0400

    Модульный тест: контроллер Cart

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
 using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.UnitTests
 {
@@ -114,6 +119,73 @@ namespace SportsStore.UnitTests
 
             // Assert
             Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Arrange - создание mock - хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+
+            // Arrange - создание Cart
+            Cart cart = new Cart();
+
+            // Arrange - создание контроллера
+            CartController target = new CartController(mock.Object);
+
+            // Act - добавление продукта в корзину
+            target.AddToCart(cart, 1, null);
+
+            // Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // Arrange - создание mock - хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+
+            // Arrange - создание Cart
+            Cart cart = new Cart();
+
+            // Arrange - создание контроллера
+            CartController target = new CartController(mock.Object);
+
+            // Act - добавление продукта в корзину
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            // Assert
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // Arrange - создание Cart
+            Cart cart = new Cart();
+
+            // Arrange - создание контроллера
+            CartController target = new CartController(null);
+
+            // Act - вызов метода действия Index
+            CartIndexViewModel result = (CartIndexViewModel)target.Index(cart, "myUrl")
+                .ViewData.Model;
+
+            // Assert
+            Assert.AreEqual(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+
         }
     }
 }
