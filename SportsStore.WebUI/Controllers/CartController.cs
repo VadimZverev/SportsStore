2cecf731023968727daa6c11a5b545774449eab6
Вадим Зверев <WadimZverev@gmail.com>
2018-08-19 19:05:09 +0400

    Завершение функционала для корзины
    -  Создание кнопки Remove;
    - Добавление виджета корзины. Добавление метода Summary  в CartController и частичного представления;
    -  добавления CSS для визуально отображения виджета.

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -50,5 +50,9 @@ namespace SportsStore.WebUI.Controllers
             return RedirectToAction("Index", new { returnUrl });
         }
 
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
     }
 }
