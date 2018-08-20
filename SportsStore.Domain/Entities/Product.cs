abe8218bf7a7357b935c31cef00d2bdd992fc276
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-20 15:35:52 +0400

    Revert "Отображаем сообщение с подтверждением"
    This reverts commit e7f729a36a313db595644317b0b9073b9344820a.

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -8,18 +8,13 @@ namespace SportsStore.Domain.Entities
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
 
-        [Required(ErrorMessage = "Введите имя продукта")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
-        [Required(ErrorMessage = "Введите описание")]
         public string Description { get; set; }
 
-        [Required]
-        [Range(0.1, double.MaxValue, ErrorMessage = "Введите положительную сумму")]
         public decimal Price { get; set; }
 
-        [Required(ErrorMessage = "Укажите категорию")]
         public string Category { get; set; }
     }
 }
