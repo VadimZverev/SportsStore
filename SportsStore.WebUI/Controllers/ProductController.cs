b78a0e6523455a8e67aa8b2d610d86921c656ba3
Вадим Зверев <WadimZverev@gmail.com>
2018-08-21 00:58:34 +0400

    Сохраняем изображения в базе данных

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -16,7 +16,7 @@ namespace SportsStore.WebUI.Controllers
 
         public ProductController(IProductRepository productRepository)
         {
-            this.repository = productRepository;
+            repository = productRepository;
         }
 
         public ViewResult List(string category, int page = 1)
@@ -40,6 +40,19 @@ namespace SportsStore.WebUI.Controllers
             };
 
             return View(model);
+        }
+
+        public FileContentResult GetImage(int productId)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
