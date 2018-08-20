a697fe773f5561f726de4fcfc4908e137a46d13d
Вадим Зверев <WadimZverev@gmail.com>
2018-08-21 00:25:02 +0400

    Модульный тест: аутентификация

---
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            // Arrange - создаем имитированного провайдера аутентификации
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+
+            // Arrange - создаём модель представления
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+
+            // Arrange - создаем контроллер
+            AccountController target = new AccountController(mock.Object);
+
+            // Act - аутентификация с использованием учётных данных
+            ActionResult result = target.Login(model, "/MyURL");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            // Arrange - создаем имитированного провайдера аутентификации
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("badUser", "badPass")).Returns(false);
+
+            // Arrange - создаём модель представления
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "badUser",
+                Password = "badPass"
+            };
+
+            // Arrange - создаем контроллер
+            AccountController target = new AccountController(mock.Object);
+
+            // Act - аутентификация с использованием учётных данных
+            ActionResult result = target.Login(model, "/MyURL");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
