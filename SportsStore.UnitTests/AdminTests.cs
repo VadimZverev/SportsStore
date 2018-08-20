76a929d42830ecb4784cc0e71983dd670bfa49fa
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-20 11:23:46 +0400

    Модульный тест: Действие Index

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+
+        [TestMethod]
+        public void Index_Contains_All_Product()
+        {
+            // Arrange - создание имитированного хранилища.
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable());
+
+            // Arrange - создание контроллера
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+
+        }
+    }
+}
